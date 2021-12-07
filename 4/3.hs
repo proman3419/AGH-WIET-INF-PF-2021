093142faeb6cdee151d4b26b116d462ed4c2a902
proman3419 <proman7268@gmail.com>
2021-12-07 10:56:12 +0100

    Finished lab4 base

--- 4/3.hs
+++ 4/3.hs
@@ -13,15 +13,21 @@ sumBinTree EmptyBT = 0
 sumBinTree (NodeBT n lt rt) = n + sumBinTree lt + sumBinTree rt
 
 data Expr a = Lit a | -- literal/value a, e.g. Lit 2 = 2
-              Add (Expr a) (Expr a)
+              Add (Expr a) (Expr a) |
+              Multiply (Expr a) (Expr a) |
+              Subtract (Expr a) (Expr a)
 
 eval :: Num a => Expr a -> a
 eval (Lit n) = n
 eval (Add e1 e2) = eval e1 + eval e2
+eval (Multiply e1 e2) = eval e1 * eval e2
+eval (Subtract e1 e2) = eval e1 - eval e2
 
 show' :: Show a => Expr a -> String
 show' (Lit n) = show n
 show' (Add e1 e2) = "(" ++ show' e1 ++ "+" ++ show' e2 ++ ")"
+show' (Multiply e1 e2) = "(" ++ show' e1 ++ "*" ++ show' e2 ++ ")"
+show' (Subtract e1 e2) = "(" ++ show' e1 ++ "-" ++ show' e2 ++ ")"
 
 depthOfBT :: BinTree a -> Int -- głębokość drzewa binarnego
 depthOfBT EmptyBT = 0
@@ -40,7 +46,20 @@ flattenBTPostorder EmptyBT = []
 flattenBTPostorder (NodeBT n lt rt) = flattenBTPostorder lt ++ flattenBTPostorder rt ++ [n]
 
 mapBT :: (a -> b) -> BinTree a -> BinTree b -- funkcja map dla drzewa binarnego
-mapBT f EmptyBT EmptyBT = BinTree EmptyBT
+mapBT f EmptyBT = EmptyBT
+mapBT f (NodeBT n lt rt) = NodeBT (f n) (mapBT f lt) (mapBT f rt) 
 
--- insert :: Ord a => a -> BinTree a -> BinTree a -- insert element into BinTree
--- list2BST :: Ord a => [a] -> BinTree a -- list to Binary Search Tree (BST)
+dummyFunc :: Num a => a -> a
+dummyFunc x = x + 5
+
+insert :: Ord a => a -> BinTree a -> BinTree a -- insert element into BinTree
+insert new EmptyBT = NodeBT new EmptyBT EmptyBT
+insert new (NodeBT n lt rt) | new < n = NodeBT n (insert new lt) rt
+                            | new > n = NodeBT n lt (insert new rt)
+                            | otherwise = NodeBT n lt rt
+
+list2BST :: Ord a => [a] -> BinTree a -- list to Binary Search Tree (BST)
+list2BST [] = EmptyBT
+list2BST (x:xs) = insert x (list2BST xs)
+
+
